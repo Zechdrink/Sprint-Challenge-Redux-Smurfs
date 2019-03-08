f1368871873780a0bcf2953d39916527539ebc94
Zech <zechdrink@aol.com>
2019-03-08 08:31:05 -0800

    added a getSmufr and addSmurf action

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,9 +1,31 @@
-export const GET_SMURF = "GET_SMURF";
-export const ADD_SMURF = "ADD_SMURF";
+import axios from 'axios';
 
+export const FETCHING_SMURF = "FETCHING_SMURF";
+export const FETCHING_SMURF_SUCCESS = "FETCHING_SMURF_SUCCESS";
+export const FETCHING_SMURF_FAIL = "FETCHING_SMURF_FAIL";
 
+export const getSmurfs = () => dispatch => {
 
+  dispatch ({ type: FETCHING_SMURF });
+  axios
+    .get('http://localhost:3333/smurfs')
+    .then( res => dispatch({ type: FETCHING_SMURF_SUCCESS, payload: res.data }))
+    .catch(err => dispatch({ type: FETCHING_SMURF_FAIL, payload: err }))
 
+}
+
+export const ADD_SMURF_START = "ADD_SMURF_START";
+export const ADD_SMURF_SUCCESS = "ADD_SMURF_SUCCESS";
+export const ADD_SMURF_FAIL = "ADD_SMURF_FAIL";
+
+export const addSmurf = newSmurf => dispatch => {
+
+  dispatch ({ type: ADD_SMURF_START });
+  axios
+    .post('http://localhost:3333/smurfs', newSmurf)
+    .then(res => dispatch({ type: ADD_SMURF_SUCCESS, payload: res.data }))
+    .catch(err => dispatch({ type: ADD_SMURF_FAIL, payload: err }))
+}
 /* 
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
